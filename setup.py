9aff3f7c33f831f6db6adaaa2c8cc4aae6fc1e69
Martin Hunt <mh12@sanger.ac.uk>
2014-07-16 09:25:14 +0100

    Increment version to 0.5.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='iva',
-    version='0.5.1',
+    version='0.5.2',
     description='Iterative Virus Assembler',
     long_description=read('README.md'),
     packages = find_packages(),
