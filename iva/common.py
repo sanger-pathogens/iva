826341886a4cf8ca5a1bb556b1fef92964e2dfee
Martin Hunt <mh12@sanger.ac.uk>
2014-07-25 10:18:21 +0100

    version 0.6.0

--- iva/common.py
+++ iva/common.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import sys
 import subprocess
-version = '0.5.2'
+version = '0.6.0'
 
 class abspathAction(argparse.Action):
     def __call__(self, parser, namespace, value, option_string):
