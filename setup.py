6983aecf234e503c09d44940939af9e15a2b8bda
Martin Hunt <mh12@sanger.ac.uk>
2015-01-07 10:38:13 +0000

    Version 0.10.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='iva',
-    version='0.10.1',
+    version='0.10.2',
     description='Iterative Virus Assembler',
     long_description=read('README.md'),
     packages = find_packages(),
