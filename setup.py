2686318b5e08fa94eddeeb2b9317c8fd9178a2db
martinghunt <mh12@sanger.ac.uk>
2015-02-05 09:24:11 +0000

    Modify for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,39 @@
 import os
 import glob
+import sys
+import shutil
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+required_progs = [
+    'kmc',
+    'kmc_dump',
+    'nucmer',
+    'delta-filter',
+    'show-coords',
+    'samtools',
+    'smalt',
+]
+
+found_all_progs = True
+print('Checking dependencies found in path:')
+for program in required_progs:
+    if shutil.which(program) is None:
+        found_all_progs = False
+        found = ' NOT FOUND'
+    else:
+        found = ' OK'
+    print(found, program, sep='\t')
+
+if not found_all_progs:
+    print('Cannot install because some dependencies not found.', file=sys.stderr)
+    sys.exit(1)
+
 
 setup(
     name='iva',
-    version='0.11.0',
+    version='0.11.1',
     description='Iterative Virus Assembler',
-    long_description=read('README.md'),
     packages = find_packages(),
     package_data={'iva': ['gage/*', 'ratt/*', 'read_trim/*']},
     author='Martin Hunt',
