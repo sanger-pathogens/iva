9d5eb9035910689e63ffe05ef3ac2d3e357b893c
Martin Hunt <mh12@sanger.ac.uk>
2015-07-09 11:47:13 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if not found_all_progs:
 
 setup(
     name='iva',
-    version='0.11.6',
+    version='0.11.7',
     description='Iterative Virus Assembler',
     packages = find_packages(),
     package_data={'iva': ['gage/*', 'ratt/*', 'read_trim/*']},
