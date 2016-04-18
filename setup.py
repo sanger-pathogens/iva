503808e3027bdadff2b362dd09a93215149f980d
Martin Hunt <mh12@sanger.ac.uk>
2016-04-18 11:42:09 +0100

    Require pysam 0.8.1-0.8.3. 0.9.0 has install issues

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
         'pyfastaq >= 3.10.0',
         'networkx >= 1.7',
-        'pysam >= 0.8.1'
+        'pysam >= 0.8.1, <= 0.8.3',
     ],
     license='GPLv3',
     classifiers=[
